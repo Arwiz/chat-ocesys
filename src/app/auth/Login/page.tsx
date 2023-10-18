02a3ba7b49224049d7d90939407828885e563510
arvindrawat <arvind@insynchro.net>
2023-10-19 04:16:32 +0530

    fix login page alignment

--- src/app/auth/Login/page.tsx
+++ src/app/auth/Login/page.tsx
@@ -4,7 +4,8 @@ import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { Button, Checkbox, Label, TextInput, Card } from 'flowbite-react';
 // import { useAuth } from '@/hooks/useauth';
-import {signIn, signOut, useSession} from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import Image from 'next/image';
 
 
 export default function Login() {
@@ -26,9 +27,10 @@ export default function Login() {
 
   };
     
-  return (
-    <div className="min-h-screen flex items-center justify-center">
+return (
+  <div className=" min-h-screen flex items-center justify-center ">
       <Card className="p-8 rounded shadow-lg max-w-md">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">OCESYS</h1>
         <h2 className="text-2xl font-semibold text-center mb-4">Login</h2>
         <form>
           <div className="mb-4">
@@ -38,7 +40,7 @@ export default function Login() {
             <input
               type="email"
               id="email"
-              className="w-full mt-1 py-2 px-3 border border-gray-300 rounded-md"
+              className="w-full mt-1 py-2 px-3 border border-gray-300  text-yellow-400 rounded-md"
               placeholder="you@example.com"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -51,7 +53,7 @@ export default function Login() {
             <input
               type="password"
               id="password"
-              className="w-full mt-1 py-2 px-3 border border-gray-300 rounded-md"
+              className="w-full mt-1 py-2 px-3 border border-gray-300  text-yellow-400 rounded-md"
               placeholder="Your password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -60,7 +62,7 @@ export default function Login() {
           <div className="mb-6">
             <button
               type="button"
-              className="w-full py-2 px-4 text-white rounded-md bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
+              className="w-full py-2 px-4 text-white rounded-md bg-yellow-400 hover:bg-yellow-600 focus:outline-none focus:ring focus:ring-blue-200"
               onClick={handleLogin}
             >
               Login
