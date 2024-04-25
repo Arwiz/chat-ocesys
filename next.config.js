d681b9e988361ec77aea68e56cf91d7d221b7c43
arvindrawat <arvind@insynchro.net>
2024-04-25 18:15:39 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    reactStrictMode: true,
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.externals.push({
+                bufferutil: 'bufferutil',
+                'utf-8-validate': 'utf-8-validate',
+                'supports-color': 'supports-color',
+            });
+        }
+
+        return config;
+    },
+};
 
 module.exports = nextConfig;
